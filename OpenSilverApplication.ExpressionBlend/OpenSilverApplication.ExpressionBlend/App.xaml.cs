618eec9b2dd78084b0b14798dffe795222bbc233
Peregrino Mendoza <pymendoza@gmail.com>
2022-02-05 14:45:14 +0800

    Initial commit

---
+++ OpenSilverApplication.ExpressionBlend/OpenSilverApplication.ExpressionBlend/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OpenSilverApplication.ExpressionBlend
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            var mainPage = new MainPage();
+            Window.Current.Content = mainPage;
+        }
+    }
+}
---
+++ OpenSilverApplication.ExpressionBlend/OpenSilverApplication.ExpressionBlend/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverApplication.ExpressionBlend
+{
+    public partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+            Label = "No background";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string label = string.Empty;
+        public string Label
+        { 
+            get
+            {
+                return label;
+            }
+            set
+            {
+                label = value;
+                RaisePropertyChangedEvent("Label");
+            }
+        }
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (label == "Green background")
+            {
+                this.Label = "Orange background";
+            }
+            else
+            {
+                this.Label = "Green background";
+            }
+        }
+    }
+}
